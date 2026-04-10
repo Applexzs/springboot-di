3346b6892b3a000106659af071ddbb89c92933e9
Applexzs <reverseapplemw2@gmail.com>
2026-04-10 17:14:22 -0600

    Agregamos servicio y controlador producto. (Funciona bien el Api)

---
+++ src/main/java/com/applexzs/springboot/di/app/springbootdi/controllers/SomeController.java
@@ -0,0 +1,28 @@
+package com.applexzs.springboot.di.app.springbootdi.controllers;
+
+
+import com.applexzs.springboot.di.app.springbootdi.models.Product;
+import com.applexzs.springboot.di.app.springbootdi.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+
+    private ProductService service = new ProductService();
+
+    @GetMapping
+    public List<Product> list(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id){
+        return service.findById(id);
+    }
+}
