d54f0259be720f9b40d00cd7777555531e4d10e6
Applexzs <reverseapplemw2@gmail.com>
2026-04-10 16:43:19 -0600

    Creamos proyecto y paquetes

---
+++ src/test/java/com/applexzs/springboot/di/app/springbootdi/SpringbootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applexzs.springboot.di.app.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
